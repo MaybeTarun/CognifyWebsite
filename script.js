df2eed5f4ddad2556e3dd38e748a650aff03a50b
Tarun Gupta <tarunthegreat2003@gmail.com>
2023-04-19 22:05:31 +0530

    Almost Completed

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
+let download = document.getElementById('download');
+
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
     if (value < window.innerHeight) {
         text.style.marginTop = value * 2.5 + 'px';
+        download.style.marginTop = value * 1.5 + 'px';
         leaf.style.top = value * -1.5 + 'px';
         leaf.style.left = value * 1.5 + 'px';
         hill5.style.left = value * 1.5 + 'px';
@@ -16,11 +19,14 @@ window.addEventListener('scroll', () => {
     }
 });
 
-var animation = bodymovin.loadAnimation({
-    container:document.getElementById('animation'),
-    path: 'he.json',
-    render:'svg',
-    loop:true,
-    autoplay: true,
-    name:'demo animation',
-});
+const scrollToTopButton = document.getElementById('totop');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
+      scrollToTopButton.style.display = 'inline-block';
+      download.style.display = 'none';
+    } else {
+      scrollToTopButton.style.display = 'none';
+      download.style.display = 'inline-block';
+    }
+  });
